ad3412b93df502f018804001da81951851b9f524
Craig <sandedshoes@gmail.com>
2022-08-08 22:01:45 +0100

    - Copper mod overlay on items - Copper Filament

--- src/main/java/mc/craig/software/craftplus/data/ModelProviderItem.java
+++ src/main/java/mc/craig/software/craftplus/data/ModelProviderItem.java
@@ -2,11 +2,14 @@ package mc.craig.software.craftplus.data;
 
 import mc.craig.software.craftplus.MinecraftPlus;
 import mc.craig.software.craftplus.common.ModItems;
+import mc.craig.software.craftplus.common.items.ParagliderItem;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModelProviderItem extends ItemModelProvider {
@@ -19,6 +22,12 @@ public class ModelProviderItem extends ItemModelProvider {
     protected void registerModels() {
         for (RegistryObject<Item> entry : ModItems.ITEMS.getEntries()) {
             if (entry.get() instanceof ForgeSpawnEggItem) continue;
+
+            if (entry.get() instanceof ParagliderItem) {
+                basicItem(new ResourceLocation(MinecraftPlus.MODID, ForgeRegistries.ITEMS.getKey(entry.get()).getPath() + "_copper_mod"));
+                continue;
+            }
+
             basicItem(entry.get());
         }
     }
--- src/main/java/mc/craig/software/craftplus/handlers/ModBusClientEvents.java
+++ src/main/java/mc/craig/software/craftplus/handlers/ModBusClientEvents.java
@@ -4,13 +4,23 @@ import mc.craig.software.craftplus.client.layers.GlideLayer;
 import mc.craig.software.craftplus.client.models.Models;
 import mc.craig.software.craftplus.client.renderers.RenderStalker;
 import mc.craig.software.craftplus.common.Entities;
+import mc.craig.software.craftplus.common.items.ParagliderItem;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Map;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModBusClientEvents {
@@ -31,6 +41,19 @@ public class ModBusClientEvents {
             LivingEntityRenderer<? extends Player, ? extends EntityModel<? extends Player>> renderer = addLayers.getSkin(skin);
             renderer.addLayer(new GlideLayer(renderer));
         });
+    }
+
+    @SubscribeEvent
+    public static void doClientStuff(FMLClientSetupEvent event) {
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> ModBusClientEvents::itemPredicates);
+    }
+
+    private static void itemPredicates() {
+        for (Map.Entry<ResourceKey<Item>, Item> entry : ForgeRegistries.ITEMS.getEntries()) {
+            if (entry.getValue() instanceof ParagliderItem paragliderItem) {
+                ItemProperties.register(paragliderItem, new ResourceLocation("copper_mod"), (stack, p_call_2_, livingEntity, something) -> ParagliderItem.hasCopperMod(stack) ? 1 : 0);
+            }
+        }
     }
 
 }
